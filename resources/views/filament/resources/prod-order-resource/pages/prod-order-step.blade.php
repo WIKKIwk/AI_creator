bbcee7d54c9eec081a91ea650babab62d501fa72
Ravshan (office) <ravshandavlatov014@gmail.com>
2025-05-08 18:07:12 +0500

    Confirm SupplyOrder creation (Insufficient error)

--- resources/views/filament/resources/prod-order-resource/pages/prod-order-step.blade.php
+++ resources/views/filament/resources/prod-order-resource/pages/prod-order-step.blade.php
@@ -1,5 +1,3 @@
 @livewire('prod-order-step-required', ['step' => $step], key('step-required-' . $step->id . '-' . now()))
 <br/>
-@livewire('prod-order-step-expected', ['step' => $step], key('step-expected-' . $step->id . '-' . now()))
-<br/>
 @livewire('prod-order-step-actual', ['step' => $step], key('step-actual-' . $step->id . '-' . now()))
