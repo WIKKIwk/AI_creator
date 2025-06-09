2d432b4c13e203a815a78025307650276e222c4e
Ravshan (office) <ravshandavlatov014@gmail.com>
2025-06-09 16:40:17 +0500

    Organization status check added

--- database/migrations/2025_06_09_112426_add_status_organizations.php
+++ database/migrations/2025_06_09_112426_add_status_organizations.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('organizations', function(Blueprint $table) {
-            $table->integer('status')->default(OrganizationStatus::ACTIVE->value);
+            $table->integer('status')->default(OrganizationStatus::Active->value);
         });
     }
 
